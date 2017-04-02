c9feec957c733ba31e840d4fd870159076fd71ef
Rudi Benkovic <rudi.benkovic@gmail.com>
2017-04-02 19:30:28 +0200

    some documentation and additional tests for timers

--- qemu_stm32_tests/src/test_timers.rs
+++ qemu_stm32_tests/src/test_timers.rs
@@ -8,16 +8,63 @@ pub extern fn test_timers() -> i8 {
 	
 	let main_task = Task::new().name("main").start(|| {
 
-        let counter_a = Arc::new(Mutex::new(0).unwrap());
-        let counter_b = Arc::new(Mutex::new(100).unwrap());
-
         {
+            let counter_a = Arc::new(Mutex::new(0).unwrap());
+            let counter_b = Arc::new(Mutex::new(100).unwrap());
+
+            {
+                let timer_a = {
+                    let counter_a = counter_a.clone();        
+                    
+                    Timer::new()
+                        .set_period(Duration::ms(50))
+                        .set_auto_reload(true)
+                        .create(move |mut timer| {                    
+                            if let Ok(mut counter_a) = counter_a.lock(Duration::ms(5)) {
+                                *counter_a += 1;
+                            }
+                        }).unwrap()
+                };
+
+                let timer_b = {
+                    let counter_b = counter_b.clone();        
+                    
+                    Timer::new()
+                        .set_period(Duration::ms(100))
+                        .set_auto_reload(false)
+                        .create(move |mut timer| {                    
+                            if let Ok(mut counter_b) = counter_b.lock(Duration::ms(5)) {
+                                *counter_b += 1;
+                            }
+                        }).unwrap()
+                };
+
+                timer_a.start(Duration::ms(1)).unwrap();
+                timer_b.start(Duration::ms(1)).unwrap();
+                
+                CurrentTask::delay(Duration::ms(225));
+
+                drop(timer_a);
+                drop(timer_b);
+            }
+            
+            let counter_a = counter_a.lock(Duration::infinite()).unwrap();
+            assert_eq!(4, *counter_a);
+
+            let counter_b = counter_b.lock(Duration::infinite()).unwrap();
+            assert_eq!(101, *counter_b);
+        }
+
+        // non-auto reloading timer test
+        {
+            let counter_a = Arc::new(Mutex::new(0).unwrap());
+
             let timer_a = {
                 let counter_a = counter_a.clone();        
                 
                 Timer::new()
                     .set_period(Duration::ms(50))
-                    .set_auto_reload(true)
+                    .set_auto_reload(false)
                     .create(move |mut timer| {                    
                         if let Ok(mut counter_a) = counter_a.lock(Duration::ms(5)) {
                             *counter_a += 1;
@@ -25,34 +72,19 @@ pub extern fn test_timers() -> i8 {
                     }).unwrap()
             };
 
-            let timer_b = {
-                let counter_b = counter_b.clone();        
-                
-                Timer::new()
-                    .set_period(Duration::ms(100))
-                    .set_auto_reload(false)
-                    .create(move |mut timer| {                    
-                        if let Ok(mut counter_b) = counter_b.lock(Duration::ms(5)) {
-                            *counter_b += 1;
-                        }
-                    }).unwrap()
-            };
+            CurrentTask::delay(Duration::ms(100));
+
+            assert_eq!(0, *counter_a.lock(Duration::infinite()).unwrap());
+            
+            timer_a.start(Duration::ms(1)).unwrap();
+
+            CurrentTask::delay(Duration::ms(200));
+            assert_eq!(1, *counter_a.lock(Duration::infinite()).unwrap());
 
             timer_a.start(Duration::ms(1)).unwrap();
-            timer_b.start(Duration::ms(1)).unwrap();
+            CurrentTask::delay(Duration::ms(200));
             
-            CurrentTask::delay(Duration::ms(225));
-
-            drop(timer_a);
-            drop(timer_b);
-        }
-        
-        if let Ok(counter_a) = counter_a.lock(Duration::infinite()) {
-            assert_eq!(4, *counter_a);
-        }
-
-        if let Ok(counter_b) = counter_b.lock(Duration::infinite()) {
-            assert_eq!(101, *counter_b);
+            assert_eq!(2, *counter_a.lock(Duration::infinite()).unwrap());
         }
 
 
